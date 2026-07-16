c157caf067edd8aeb411d906f165742023169717
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-16 13:37:30 +0530

    clouds

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,15 +77,21 @@ export default function RootLayout({
         </noscript>
         {/* End Meta Pixel Code */}
 
-        {/* Add your custom tag here */}
-        <script
+        {/* Microsoft Clarity Tracking Code */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              // Your custom JavaScript code here
-              console.log('Custom script loaded');
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xn6rw6vyex");
             `,
           }}
         />
+        {/* End Microsoft Clarity Tracking Code */}
       </body>
     </html>
   );
